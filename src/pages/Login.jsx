d5c12bb67689fb90f8355603f97689bdcab6bd06
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-06 13:03:42 +0200

    feat: Login component displays both 400 and 401 validation errors to user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,14 @@
-import { Form } from "react-router-dom";
+import { Form, useActionData } from "react-router-dom";
 
 export default function Login() {
+  const errors = useActionData();
+
   return (
     <>
       <h1>Login</h1>
+      {errors?.length > 0 && errors.map((error, index) => {
+        return <li key={index} className="text-red-400">{error}</li>;
+      })}
       <Form method="post">
         <div>
           <label htmlFor="email">Email</label>
