b7cd2ed743faf50f8e038927b55f4a8f6ede0da3
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-10 17:51:08 +0200

    feat: add auth context and provider for user, token and login to the app
    App now has centralized state available to all components for user, token and login functionality.

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect, createContext, useContext } from "react";
+import { jwtDecode } from "jwt-decode";
+
+const AuthContext = createContext({
+
+});
+
+const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState(null);
+	const [token, setToken] = useState(localStorage.getItem("accessToken" || null));
+
+	// Request user login
+  const login = async (email, password) => {
+		try {
+			const response = await axios.post(`${API_URL}/auth/login`, {
+				email,
+				password,
+			});
+
+			const { accessToken, refreshToken } = response.data;
+			// Store access and refresh tokens in localStorage
+			localStorage.setItem("accessToken", accessToken);
+			localStorage.setItem("refreshToken", refreshToken);
+			// Set access token state
+			setToken(accessToken);
+			// Decode and set user state
+			const user = jwtDecode(accessToken);
+			setUser(user);
+			return { success: true };
+		} catch (error) {
+			// Return form validation error/s
+			if (error.status === 400) {
+				return { success: false, error: error.response.data.details };
+			};
+			// Return unauthorized error message
+			if (error.status === 401) {
+				return { success: false, error: [error.response.data.message] };
+			};
+			// Otherwise let the error boundary catch it
+			throw error;
+		};
+	};
+
+	const value = {
+		login
+	};
+
+	return (
+		<AuthContext.Provider value={value}>
+			{children}
+		</AuthContext.Provider>);
+};
+
+export default AuthProvider;
+
+export const useAuth = () => {
+	return useContext(AuthContext);
+};
