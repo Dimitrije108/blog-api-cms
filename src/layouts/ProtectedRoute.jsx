ebbb0d5e7ecaa4a5786b51eb959cfbfa7220f03d
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-13 13:21:23 +0200

    feat: update protected route component to include auth provider on load and to render based on user authentication

--- src/layouts/ProtectedRoute.jsx
+++ src/layouts/ProtectedRoute.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthProvider';
 
+// Checks user auth so that page isn't displayed to non authenticated users
 export default function ProtectedRoute() {
-	const isAuthenticated = !!localStorage.getItem('accessToken');
+	const [isAuth, setIsAuth] = useState(null);
+	const { authUser } = useAuth();
 
-	if (!isAuthenticated) {
+	// Check user auth
+	useEffect(() => {
+		async function checkAuth() {
+			const userAuth = await authUser();
+			setIsAuth(userAuth);
+		};
+		checkAuth();
+	}, [authUser]);
+	
+	// Authentication in progress
+	if (isAuth === null) {
+		return <div>Loading...</div>;
+	};
+	// Load children if authenticated
+	if (isAuth) {
+		return <Outlet />;
+	};
+	// Redirect to login if not authenticated
+	if (!isAuth) {
 		return <Navigate to='/login' replace />;
 	};
-
-	return <Outlet />;
 };
