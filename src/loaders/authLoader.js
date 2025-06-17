53ea1536740e9e34ada7654e7d81140a41fcfb06
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-17 14:05:02 +0200

    fix: authLoader function returing a reposponse instead of the loader function

--- src/loaders/authLoader.js
+++ src/loaders/authLoader.js
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom";
 import checkAuth from "../utils/checkAuth";
 
 // Check if user is authenticated before initiating route's loader
-export default async function authLoader(loader) {
+export default function authLoader(loader) {
 	// Return a function that will execute when route loader is accessed
 	return async (...args) => {
 		const isAuth = await checkAuth();
