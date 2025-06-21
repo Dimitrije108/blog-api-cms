dd77db25d3cd6154d863ed3881aa1f90e834b0c2
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-21 17:47:30 +0200

    feat: add form action for article submission with error handling

---
+++ src/features/createArticle/action.js
@@ -0,0 +1,37 @@
+import api from "../../axiosConfig";
+
+const handleError = (error) => {
+  const status = error.response?.status;
+  // Return form validation error/s
+  if (status === 400) {
+    return { error: error.response.data.details };
+  };
+  // Otherwise let the error boundary catch it
+  throw error;
+};
+
+export default async function createArticleAction({ request }) {
+  // Get form data
+  const formData = await request.formData();
+  const title = formData.get("title");
+	const content = formData.get("content");
+	const categoryId = formData.get("category");
+	// Check if published was checked
+	const published = formData.get("published") === "on";
+	// Check if content length is good
+	if (content.length < 1000) {
+		return { error: ["Content must be between 1000 and 10000 characters long"] };
+	};
+
+  // Create new article
+	try {
+		const response = await api.post(
+			'/articles', 
+			{ title, content, categoryId, published }
+		);
+		// Return success message
+		return { data: response.data };
+	} catch (error) {
+		return handleError(error);
+	};
+};
