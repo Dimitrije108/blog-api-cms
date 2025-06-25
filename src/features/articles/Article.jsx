8c4952eba33b7586dbf05723d439fd9daf9bf526
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-25 16:25:28 +0200

    feat: add article component to display single article

---
+++ src/features/articles/Article.jsx
@@ -0,0 +1,30 @@
+import { useLoaderData } from "react-router-dom";
+import ErrorMessage from "../../components/ErrorMessage";
+
+// TODO:
+// - Format created at date
+// - Position all elements properly
+
+export default function Article() {
+	const { data, error } = useLoaderData();
+	const content = { __html: data.content };
+
+	return (
+		<div className="flex justify-center items-center">
+			{error && <ErrorMessage error={error} />}
+			<article className="tinymce-content max-w-[70ch]">
+				<h1 className="self-center">{data.title}</h1>
+				{!data.published && <p className="text-xl text-red-400">Unpublished</p>}
+				<div className="w-full flex justify-between items-end">
+					<p className="mt-2 font-['Aulieny'] text-3xl self-start">By {data.user.username}</p>
+					<div className="flex flex-col text-sm">
+						<span>Category: {data.category.name}</span>
+						<span>Created {data.createdAt}</span>
+					</div>
+				</div>
+				<hr />
+				<section dangerouslySetInnerHTML={content}></section>
+			</article>
+		</div>
+	);
+};
