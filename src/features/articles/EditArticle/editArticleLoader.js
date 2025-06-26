5dd449b7f087454421692de7419473a06c7d49bf
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-26 17:54:26 +0200

    feat: implement article loader by combining article and categories loaders

---
+++ src/features/articles/EditArticle/editArticleLoader.js
@@ -0,0 +1,9 @@
+import articleLoader from "../ArticleDetails/articleLoader";
+import categoriesLoader from "../../categories/loader";
+// Call article and categories loader to fetch all info needed to edit article
+export default async function editArticleLoader({ params }) {
+	const article = await articleLoader({ params });
+	const categories = await categoriesLoader();
+	
+	return { article, categories };
+};
