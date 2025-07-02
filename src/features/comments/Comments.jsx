0863ae330407bed990be83b2cfe7ec34fcc2db7d
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-07-02 18:45:05 +0200

    feat: add comments component to display all comments with delete functionality

---
+++ src/features/comments/Comments.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useLoaderData, useRevalidator } from "react-router-dom";
+import api from "../../axiosConfig";
+import ErrorMessage from "../../components/ErrorMessage";
+
+// TODO:
+// - Search by user
+// - Show updatedAt if comment was updated - if I can get it to work since Prisma's 
+// implementation is not very smart to say the least
+
+export default function Comments() {
+  const { data, error } = useLoaderData();
+  const [deleteError, setDeleteError] = useState(null);
+  const revalidator = useRevalidator();
+
+  // Delete comment and refresh users loader
+  const handleDelete = async (commentId) => {
+    // Confirm popup to delete a user
+    const confirm = window.confirm("Are you sure you want to delete this user?");
+
+    if (confirm) {
+      try {
+        await api.delete(`/comments/${commentId}`);
+        revalidator.revalidate();
+        setDeleteError(null);
+      } catch(error) {
+        setDeleteError(error);
+      };
+    };
+  };
+
+  return (
+    <>
+      {deleteError && <ErrorMessage error={deleteError} />}
+      {error && <ErrorMessage error={error} />}
+      {data && 
+        <div className="max-w-3xl m-auto">
+          <ul>
+            {data.map((comment) => {
+              return (
+                <li key={comment.id}>
+                  <div>{comment.user.username}</div>
+                  {comment.user.author && <div>Author</div>}
+                  <div>{comment.comment}</div>
+                  <div>{comment.createdAt}</div>
+                  <div>Article: {comment.article.title}</div>
+                  <button onClick={() => handleDelete(comment.id)}>
+                    Delete
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      }
+    </>
+  )
+};
