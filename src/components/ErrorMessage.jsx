d25b4e210af84795578c5b1a526f7e510708e69d
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-07-04 18:24:49 +0200

    feat: adjust error message component to better handle displaying errors

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,17 +1,24 @@
-export default function ErrorMessage({ status, error, message, details }) {
+export default function ErrorMessage({ error }) {
+	const status = error.status;
+	const { error: name, message, details } = error.response.data;
+
 	return (
-		<>
-			<div>{status}</div>
+		<div className="flex flex-col justify-center items-center text-red-500">
 			<div className="flex">
-				<p>{error}</p>
-				<p>{message}</p>
+				<div className="pr-1">{status}</div>
+				<p>{name}</p>
 			</div>
-			{details && details.map((err) => (
-				<div className="flex">
-					<p>{err.field}</p>
-					<li>{err.message}</li>
-				</div>
-			))}
-		</>
+			<p>{message}</p>
+			{details && details?.length > 0 &&
+				<ul>
+					{details.map((err, index) => (
+						<li key={index} className="flex">
+							<p>{err.field}</p>
+							<span>{err.message}</span>
+						</li>
+					))}
+				</ul>
+			}
+		</div>
 	);
 };
