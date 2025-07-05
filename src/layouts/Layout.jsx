4c8217c257b3fa264e6dada04ac22ccd6fd9d0fe
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-07-05 18:18:20 +0200

    feat: add component loading check to the layout and implement loading spinner to layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,14 +1,23 @@
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import { useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Spinner from "../components/Spinner";
 
 export default function Layout() {
+  const navigation = useNavigation();
+  // Check for loading state
+  const isLoading = navigation.state === "loading" || navigation.state === "submitting";
+
   return (
-    <div className='mx-auto lg:max-w-[90rem] h-screen grid grid-cols-[auto_1fr] grid-rows-[auto_1fr]'>
+    <div className="mx-auto lg:max-w-[90rem] h-screen grid grid-cols-[auto_1fr] grid-rows-[auto_1fr]">
       <Sidebar />
       <Header />
-      <main className='p-6'>
-        <Outlet />
+      <main className="pl-10 pr-10 pb-14">
+        {isLoading
+          ? <Spinner />
+          : <Outlet />
+        }
       </main>
     </div>
   )
