257502a6183b58bea46052484cdb1952ee49c133
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-07-05 12:31:04 +0200

    feat: implement custom active style for sidebar navbar links

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,7 +1,10 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 export default function Sidebar() {
+  const { pathname } = useLocation();
+  const activeStyle = "text-black";
+
   return (
     <div className="pt-4 pr-16 pb-4 pl-16 bg-white row-span-2 border-r border-r-gray-300 shadow-[8px_0_15px_-3px_rgba(0,0,0,0.1),8px_0_6px_-2px_rgba(0,0,0,0.05)]">
       <img
@@ -13,24 +16,54 @@ export default function Sidebar() {
         height={100}
       />
       <nav>
-        <ul className='space-y-2 divide-y divide-gray-300 font-["Oswald-Light"] text-xl tracking-wider [&>li]:pb-2 [&>li]:text-gray-500 [&>li]:transition-all [&>li]:duration-100 [&>li]:hover:text-gray-900'>
+        <ul className="space-y-2 divide-y divide-gray-300 font-['Oswald-Light'] text-xl tracking-wider [&>li]:pb-2 [&>li]:text-gray-500 [&>li]:transition-all [&>li]:duration-100 [&>li]:hover:text-gray-900">
           <li>
-            <Link to="/">Dashboard</Link>
+            <NavLink 
+              to="/" 
+              className={pathname === "/" ? activeStyle : ""}
+            >
+              Dashboard
+            </NavLink>
           </li>
           <li>
-            <Link to="/articles">Articles</Link>
+            <NavLink 
+              to="/articles" 
+              className={pathname.startsWith("/articles") && pathname !== "/articles/create" ? activeStyle : ""}
+            >
+              Articles
+            </NavLink>
           </li>
           <li>
-            <Link to="/articles/create">Create Article</Link>
+            <NavLink 
+              to="/articles/create" 
+              className={pathname === "/articles/create" ? activeStyle : ""}
+            >
+              Create Article
+            </NavLink>
           </li>
           <li>
-            <Link to="/categories">Categories</Link>
+            <NavLink 
+              to="/categories" 
+              className={pathname === "/categories" ? activeStyle : ""}
+            >
+              Categories
+            </NavLink>
           </li>
           <li>
-            <Link to="/comments">Comments</Link>
+            <NavLink 
+              to="/comments" 
+              className={pathname === "/comments" ? activeStyle : ""}
+            >
+              Comments
+            </NavLink>
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <NavLink 
+              to="/users" 
+              className={pathname === "/users" ? activeStyle : ""}
+            >
+              Users
+            </NavLink>
           </li>
         </ul>
       </nav>
